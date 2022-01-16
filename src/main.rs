c5644f9c59424081c63477a9bbc0948117405d02
Sameer Puri <sameer@users.noreply.github.com>
2022-01-16 10:33:41 -0800

    move State below main()

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,55 @@ use wayland_protocols::unstable::idle_inhibit::v1::client::{
 /// will reduce CPU usage while still achieving the desired effect.
 const PLAYER_POLL_SLEEP_DURATION: Duration = Duration::from_secs(5);
 
+fn main() {
+    let player_finder = PlayerFinder::new().expect("could not connect to DBus");
+
+    let conn = Connection::connect_to_env().unwrap();
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+    let display = conn.handle().display();
+
+    let _registry = display.get_registry(&mut conn.handle(), &qh, ()).unwrap();
+
+    let mut state = State::default();
+    event_queue.blocking_dispatch(&mut state).unwrap();
+    let mut idle_inhibitor = None;
+    loop {
+        let active_player_opt = player_finder
+            .find_all()
+            .unwrap()
+            .drain(..)
+            .find(|p| p.get_playback_status().unwrap() == PlaybackStatus::Playing);
+        if let Some(active_player) = active_player_opt {
+            idle_inhibitor = idle_inhibitor.or_else(|| {
+                Some(
+                    state
+                        .idle_inhibit_manager
+                        .as_ref()
+                        .unwrap()
+                        .create_inhibitor(&mut conn.handle(), state.surf.as_ref().unwrap(), &qh, ())
+                        .unwrap(),
+                )
+            });
+            println!("Idle inhibited by {}", active_player.identity());
+            // Blocks until new events are received.
+            // Guaranteed to (eventually) receive a shutdown event which will break this loop.
+            while !active_player.events().unwrap().any(|event| {
+                matches!(
+                    event.unwrap(),
+                    MprisEvent::PlayerShutDown | MprisEvent::Stopped | MprisEvent::Paused
+                )
+            }) {}
+            // Allows immediate decision on whether to destroy idle inhibitor
+            continue;
+        } else if let Some(idle_inhibitor) = idle_inhibitor.as_ref() {
+            idle_inhibitor.destroy(&mut conn.handle());
+            println!("Idle allowed");
+        }
+        thread::sleep(PLAYER_POLL_SLEEP_DURATION)
+    }
+}
+
 #[derive(Default)]
 struct State {
     compositor: Option<WlCompositor>,
@@ -120,54 +169,5 @@ impl Dispatch<ZwpIdleInhibitorV1> for State {
         _: &mut ConnectionHandle,
         _: &QueueHandle<Self>,
     ) {
-    }
-}
-
-fn main() {
-    let player_finder = PlayerFinder::new().expect("could not connect to DBus");
-
-    let conn = Connection::connect_to_env().unwrap();
-    let mut event_queue = conn.new_event_queue();
-    let qh = event_queue.handle();
-    let display = conn.handle().display();
-
-    let _registry = display.get_registry(&mut conn.handle(), &qh, ()).unwrap();
-
-    let mut state = State::default();
-    event_queue.blocking_dispatch(&mut state).unwrap();
-    let mut idle_inhibitor = None;
-    loop {
-        let active_player_opt = player_finder
-            .find_all()
-            .unwrap()
-            .drain(..)
-            .find(|p| p.get_playback_status().unwrap() == PlaybackStatus::Playing);
-        if let Some(active_player) = active_player_opt {
-            idle_inhibitor = idle_inhibitor.or_else(|| {
-                Some(
-                    state
-                        .idle_inhibit_manager
-                        .as_ref()
-                        .unwrap()
-                        .create_inhibitor(&mut conn.handle(), state.surf.as_ref().unwrap(), &qh, ())
-                        .unwrap(),
-                )
-            });
-            println!("Idle inhibited by {}", active_player.identity());
-            // Blocks until new events are received.
-            // Guaranteed to (eventually) receive a shutdown event which will break this loop.
-            while !active_player.events().unwrap().any(|event| {
-                matches!(
-                    event.unwrap(),
-                    MprisEvent::PlayerShutDown | MprisEvent::Stopped | MprisEvent::Paused
-                )
-            }) {}
-            // Allows immediate decision on whether to destroy idle inhibitor
-            continue;
-        } else if let Some(idle_inhibitor) = idle_inhibitor.as_ref() {
-            idle_inhibitor.destroy(&mut conn.handle());
-            println!("Idle allowed");
-        }
-        thread::sleep(PLAYER_POLL_SLEEP_DURATION)
     }
 }
