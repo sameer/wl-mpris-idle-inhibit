51e2f935a9b4a6f0e11706b9d525ac091e4362f3
Sameer Puri <sameer@users.noreply.github.com>
2022-06-20 11:03:47 -0700

    update wayland-protocols + wayland-client to 0.30.0-beta.5

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ use wayland_client::{
         wl_registry::{self, WlRegistry},
         wl_surface::{self, WlSurface},
     },
-    Connection, ConnectionHandle, Dispatch, QueueHandle,
+    Connection, Dispatch, QueueHandle,
 };
-use wayland_protocols::unstable::idle_inhibit::v1::client::{
+use wayland_protocols::wp::idle_inhibit::zv1::client::{
     __interfaces::ZWP_IDLE_INHIBIT_MANAGER_V1_INTERFACE,
     zwp_idle_inhibit_manager_v1::{self, ZwpIdleInhibitManagerV1},
     zwp_idle_inhibitor_v1::{self, ZwpIdleInhibitorV1},
@@ -32,9 +32,9 @@ fn main() {
     let conn = Connection::connect_to_env().expect("could not connect to Wayland server");
     let mut event_queue = conn.new_event_queue();
     let qh = event_queue.handle();
-    let display = conn.handle().display();
+    let display = conn.display();
 
-    let _registry = display.get_registry(&mut conn.handle(), &qh, ()).unwrap();
+    let _registry = display.get_registry(&qh, ()).unwrap();
 
     let mut state = State::default();
     event_queue.blocking_dispatch(&mut state).unwrap();
@@ -51,7 +51,6 @@ fn main() {
                     .as_ref()
                     .expect("idle manager should be present")
                     .create_inhibitor(
-                        &mut conn.handle(),
                         state
                             .surf
                             .as_ref()
@@ -98,7 +97,7 @@ fn main() {
                 }
             }
         } else if let Some(i) = idle_inhibitor.as_ref() {
-            i.destroy(&mut conn.handle());
+            i.destroy();
             idle_inhibitor = None;
             conn.roundtrip()
                 .expect("failed to request destruction of idle inhibitor");
@@ -109,9 +108,9 @@ fn main() {
 }
 
 /// Returns the first active player that is found.
-/// 
+///
 /// Returns [Ok(None)] when there are no active players
-/// and the playerctld daemon returns a D-Bus error. 
+/// and the playerctld daemon returns a D-Bus error.
 fn find_active_player(player_finder: &PlayerFinder) -> Result<Option<Player>, FindingError> {
     let res = player_finder.find_all().map(|players| {
         players.into_iter().find(|p| match p.get_playback_status() {
@@ -140,15 +139,13 @@ struct State {
     idle_inhibit_manager: Option<ZwpIdleInhibitManagerV1>,
 }
 
-impl Dispatch<WlRegistry> for State {
-    type UserData = ();
-
+impl Dispatch<WlRegistry, ()> for State {
     fn event(
         &mut self,
         registry: &WlRegistry,
         event: wl_registry::Event,
-        _: &Self::UserData,
-        conn: &mut ConnectionHandle,
+        _: &(),
+        _conn: &Connection,
         qh: &QueueHandle<Self>,
     ) {
         match event {
@@ -158,9 +155,9 @@ impl Dispatch<WlRegistry> for State {
                 version,
             } if interface == WL_COMPOSITOR_INTERFACE.name => {
                 let compositor = registry
-                    .bind::<WlCompositor, _>(conn, name, version, qh, ())
+                    .bind::<WlCompositor, _, _>(name, version, qh, ())
                     .unwrap();
-                self.surf = Some(compositor.create_surface(conn, qh, ()).unwrap());
+                self.surf = Some(compositor.create_surface(qh, ()).unwrap());
                 self.compositor = Some(compositor);
                 eprintln!("[{}] {} (v{})", name, interface, version);
             }
@@ -170,7 +167,7 @@ impl Dispatch<WlRegistry> for State {
                 version,
             } if interface == ZWP_IDLE_INHIBIT_MANAGER_V1_INTERFACE.name => {
                 let idle_inhibit_manager = registry
-                    .bind::<ZwpIdleInhibitManagerV1, _>(conn, name, version, qh, ())
+                    .bind::<ZwpIdleInhibitManagerV1, _, _>(name, version, qh, ())
                     .unwrap();
                 self.idle_inhibit_manager = Some(idle_inhibit_manager);
                 eprintln!("[{}] {} (v{})", name, interface, version);
@@ -181,57 +178,49 @@ impl Dispatch<WlRegistry> for State {
     }
 }
 
-impl Dispatch<WlCompositor> for State {
-    type UserData = ();
-
+impl Dispatch<WlCompositor, ()> for State {
     fn event(
         &mut self,
         _: &WlCompositor,
         _: wl_compositor::Event,
-        _: &Self::UserData,
-        _: &mut ConnectionHandle,
+        _: &(),
+        _: &Connection,
         _: &QueueHandle<Self>,
     ) {
     }
 }
 
-impl Dispatch<WlSurface> for State {
-    type UserData = ();
-
+impl Dispatch<WlSurface, ()> for State {
     fn event(
         &mut self,
         _: &WlSurface,
         _: wl_surface::Event,
-        _: &Self::UserData,
-        _: &mut ConnectionHandle,
+        _: &(),
+        _: &Connection,
         _: &QueueHandle<Self>,
     ) {
     }
 }
 
-impl Dispatch<ZwpIdleInhibitManagerV1> for State {
-    type UserData = ();
-
+impl Dispatch<ZwpIdleInhibitManagerV1, ()> for State {
     fn event(
         &mut self,
         _: &ZwpIdleInhibitManagerV1,
         _: zwp_idle_inhibit_manager_v1::Event,
-        _: &Self::UserData,
-        _: &mut ConnectionHandle,
+        _: &(),
+        _: &Connection,
         _: &QueueHandle<Self>,
     ) {
     }
 }
 
-impl Dispatch<ZwpIdleInhibitorV1> for State {
-    type UserData = ();
-
+impl Dispatch<ZwpIdleInhibitorV1, ()> for State {
     fn event(
         &mut self,
         _: &ZwpIdleInhibitorV1,
         _: zwp_idle_inhibitor_v1::Event,
-        _: &Self::UserData,
-        _: &mut ConnectionHandle,
+        _: &(),
+        _: &Connection,
         _: &QueueHandle<Self>,
     ) {
     }
